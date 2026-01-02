1682a6068ff0052d7229269826c6bead411cbd2d
Razanhaweizy <Razanrh05@gmail.com>
2026-01-03 02:58:41 +0300

    users can see how many dice they rolled

--- roll.py
+++ roll.py
@@ -1,9 +1,14 @@
 import random
 
 terminate = False
+dice_counter = 0
 
 while not terminate:
-    user_input = input("Roll the dice? ").lower()
+    if dice_counter == 50:
+        print("Wow, you're locked in... you have officially rolled 50 times")
+
+    user_input = input("Roll the dice? Type c to view amount of dice rolled ").lower()
+
     if user_input == "y":
         dices = int(input("How many dice to roll? "))
         rolls = []
@@ -11,11 +16,15 @@ while not terminate:
         for i in range(dices):
             rand_roll = random.randint(1, 6)
             rolls.append(str(rand_roll))
+            dice_counter += 1
         res.join(rolls)
         print(f'({", ".join(rolls)})')
     elif user_input == "n":
         print("Thanks for playing!")
         terminate = True
+        continue
+    elif user_input == "c":
+        print(f"You have rolled {dice_counter} dices >_<")
         continue
     else:
         print("Invalid choice :P")
