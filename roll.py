61e1bac483162c7e7543f834d98e7ebc1e5d88f8
Razanhaweizy <Razanrh05@gmail.com>
2026-01-03 17:07:33 +0300

    Removed unnecessary variable

--- roll.py
+++ roll.py
@@ -5,19 +5,17 @@ dice_counter = 0
 
 while not terminate:
     if dice_counter == 50:
-        print("Wow, you're locked in... you have officially rolled 50 times")
+        print("Wow, you're locked in... you have officially rolled 100 times")
 
     user_input = input("Roll the dice? Type c to view amount of dice rolled ").lower()
 
     if user_input == "y":
         dices = int(input("How many dice to roll? "))
         rolls = []
-        res = ""
         for i in range(dices):
             rand_roll = random.randint(1, 6)
             rolls.append(str(rand_roll))
             dice_counter += 1
-        res.join(rolls)
         print(f'({", ".join(rolls)})')
     elif user_input == "n":
         print("Thanks for playing!")
@@ -29,6 +27,3 @@ while not terminate:
     else:
         print("Invalid choice :P")
         continue
-
-
-
